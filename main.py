fa28068a08bb0ea09fbdc96e94c361133eb7ca37
claudi47 <claudiosicari99@gmail.com>
2026-04-29 16:23:53 +0200

    add double model and test input prompt

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
 
 def main():
     print("Hello from langchain-course!")
+    information = """
+    Elon Reeve Musk[1] (AFI:[ˈiːlɒn ˈɹiːv ˈmʌsk]) (Pretoria, 28 giugno 1971) è un imprenditore e politico sudafricano con cittadinanza canadese naturalizzato statunitense.
+    Ricopre i ruoli di fondatore, amministratore delegato e direttore tecnico della compagnia aerospaziale SpaceX, fondatore di The Boring Company e della società di intelligenza artificiale xAI, cofondatore di Neuralink e OpenAI, amministratore delegato e product architect della multinazionale automobilistica Tesla, proprietario e presidente di X (precedentemente Twitter). Ha inoltre proposto un sistema di trasporto superveloce conosciuto come Hyperloop One, posta in liquidazione il 21 dicembre 2023. Tramite SpaceX gestisce Starlink, una costellazione satellitare che avrebbe l'obiettivo di fornire Internet ad alta velocità e bassa latenza a tutto il pianeta.
+    Secondo Forbes, al 7 febbraio 2026, con un patrimonio stimato di 849,3 miliardi di dollari, risulta essere la persona più ricca del mondo.
+    Dal 20 gennaio al 29 maggio 2025 è stato a capo del Dipartimento dell'Efficienza Governativa statunitense.
+    """
+    summary_template = """
+    Given the information {information}, about a person I want you to create:
+    1. A short summary
+    2. Two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+    )
+
+    llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+    # llm = ChatOllama(temperature=0, model="gemma3:4b")
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
