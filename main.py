6079f7b4d751eeddff10e6d4c1faca2f212c6e31
claudi47 <claudiosicari99@gmail.com>
2026-05-04 14:55:56 +0200

    Added realt-time search for mocked question

--- main.py
+++ main.py
@@ -3,29 +3,20 @@ from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
-from langchain.tools import tool
+from langchain_tavily import TavilySearch
 from langchain_core.messages import HumanMessage
 
 load_dotenv()
 
-
-def search_web(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query (str): The query to search for
-    Returns:
-        the search results"""
-    print(f"Searching web for: {query}")
-    return f"Search results for '{query}'"
-
 llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
-tools = [search_web]
+tools = [TavilySearch(max_results=3)]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": [HumanMessage(content="What is the capital of France?")]})
+    result = agent.invoke({
+        "messages": [HumanMessage(content="What is the actual weather in Beijing?")]
+    })
     print(result)
 
 
